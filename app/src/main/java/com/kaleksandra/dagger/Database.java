6cd2603d99aa4845c74003d5a09612cfdc13eb29
Aleksandra <portos100.00.1a@gmail.com>
2022-04-14 15:35:07 +0300

    Added Database class

---
+++ app/src/main/java/com/kaleksandra/dagger/Database.java
@@ -0,0 +1,40 @@
+package com.kaleksandra.dagger;
+
+import android.annotation.TargetApi;
+import android.os.Build;
+
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.inject.Inject;
+
+class Database {
+    private final Map<String, Account> accounts = new HashMap<>();
+
+    @Inject
+    Database() {
+    }
+
+    @TargetApi(Build.VERSION_CODES.N)
+    Account getAccount(String username) {
+        return accounts.computeIfAbsent(username, Account::new);
+    }
+
+    static final class Account {
+        private final String username;
+        private BigDecimal balance = BigDecimal.ZERO;
+
+        Account(String username) {
+            this.username = username;
+        }
+
+        String username() {
+            return username;
+        }
+
+        BigDecimal balance() {
+            return balance;
+        }
+    }
+}
